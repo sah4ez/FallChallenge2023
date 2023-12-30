4883195d87275998a4f174a926c4ec9a2abfd55f
Alexandr Kozlenkov <sah4ez32@gmail.com>
2023-12-30 15:10:26 +0300

    Added debug position

--- game/main.go
+++ game/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Creature struct {
@@ -45,12 +46,18 @@ func (d *Drone) Light() string {
 	return "0"
 }
 
-func (d *Drone) Wait() {
-	fmt.Printf("WAIT %s\n", d.Light())
+func (d *Drone) Wait(msg ...string) {
+	if len(msg) == 0 {
+		msg = append(msg, fmt.Sprintf("%d %d", d.X, d.Y))
+	}
+	fmt.Printf("WAIT %s %s\n", d.Light(), strings.Join(msg, " "))
 }
 
-func (d *Drone) Move(x, y int) {
-	fmt.Printf("MOVE %d %d %s\n", x, y, d.Light())
+func (d *Drone) Move(x, y int, msg ...string) {
+	if len(msg) == 0 {
+		msg = append(msg, fmt.Sprintf("%d %d", d.X, d.Y))
+	}
+	fmt.Printf("MOVE %d %d %s %s\n", x, y, d.Light(), strings.Join(msg, " "))
 }
 
 func (d *Drone) Debug() {
--- src/drone.go
+++ src/drone.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
@@ -31,12 +32,18 @@ func (d *Drone) Light() string {
 	return "0"
 }
 
-func (d *Drone) Wait() {
-	fmt.Printf("WAIT %s\n", d.Light())
+func (d *Drone) Wait(msg ...string) {
+	if len(msg) == 0 {
+		msg = append(msg, fmt.Sprintf("%d %d", d.X, d.Y))
+	}
+	fmt.Printf("WAIT %s %s\n", d.Light(), strings.Join(msg, " "))
 }
 
-func (d *Drone) Move(x, y int) {
-	fmt.Printf("MOVE %d %d %s\n", x, y, d.Light())
+func (d *Drone) Move(x, y int, msg ...string) {
+	if len(msg) == 0 {
+		msg = append(msg, fmt.Sprintf("%d %d", d.X, d.Y))
+	}
+	fmt.Printf("MOVE %d %d %s %s\n", x, y, d.Light(), strings.Join(msg, " "))
 }
 
 func (d *Drone) Debug() {
